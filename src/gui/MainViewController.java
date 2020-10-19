467e12b60ea261d58d4a9d5ce63ec45412120fd2
Lucas Pacheco <lucas.vinicius719@gmail.com>
2020-10-19 11:57:09 -0300

    DepartmentForm

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -68,6 +68,7 @@ public class MainViewController implements Initializable {
             initializingAction.accept(controller);
         }catch (IOException e){
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+            e.printStackTrace();
         }
     }
 

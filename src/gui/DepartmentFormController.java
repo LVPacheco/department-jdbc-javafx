2577d4aae118b143827b2487ec1c5fec1e0112fc
Lucas Pacheco <lucas.vinicius719@gmail.com>
2020-10-19 15:18:20 -0300

    Validation Exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -12,13 +12,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 import javafx.event.ActionEvent;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class DepartmentFormController implements Initializable {
 
@@ -48,6 +47,8 @@ public class DepartmentFormController implements Initializable {
             Utils.currentStage(event).close();
         }catch (DbException e){
             Alerts.showAlert("Error saving object", null,e.getMessage(), Alert.AlertType.ERROR);
+        }catch (ValidationException e){
+            setErrorMessages(e.getErrors());
         }
 
     }
@@ -58,9 +59,11 @@ public class DepartmentFormController implements Initializable {
 
     private Department getFormData() {
         Department obj = new Department();
+        ValidationException exception = new ValidationException("Validation error");
         obj.setId(Utils.tryParsetoInt(tfId.getText()));
+        if(tfName.getText()==null||tfName.getText().equals("")) exception.addError("name", "Field can't be empty");
         obj.setName(tfName.getText());
-        if(tfName.getText().equals("")) throw new IllegalStateException("Name cannot be null");
+        if(exception.getErrors().size()>0) throw exception;
         return obj;
     }
 
@@ -98,6 +101,11 @@ public class DepartmentFormController implements Initializable {
 
     public void subscribeDataChangeListener(DataChangeListener listener){
         dataChangeListeners.add(listener);
+    }
+
+    private void setErrorMessages(Map<String, String> errors){
+        Set<String> fields = errors.keySet();
+        if(fields.contains("name")) lbError.setText(errors.get("name"));
     }
 
 }
